fc22aae0b92a94c2352c379904bcf47b9fd87b46
unknown <akhil.mr2021@gmail.com>
2025-03-19 08:38:04 +0530

    q

---
+++ remove_ele.cpp
@@ -0,0 +1,9 @@
+int removeElement(vector<int>& nums, int val) {
+    for(auto i = nums.begin(); i != nums.end();){
+        if(*i == val){
+            i = nums.erase(i);
+        }
+        else i++;
+    }
+    return nums.size();
+}
